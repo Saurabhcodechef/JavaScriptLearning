202df7c82eda7f80431da51b08cb1548959a38f6
Saurabh Sharma <59047214+Saurabhcodechef@users.noreply.github.com>
2023-11-27 03:19:21 +0000

    File Modified

--- Lecture01/test.js
+++ Lecture01/test.js
@@ -1,1 +1,10 @@
-console.log("Saurabh Sharma");
+const accountId = "123243"
+let accountName = "Saurabh Sharma"
+var accountPassword = "121343"
+accountCity = "Bareilly"
+let accountState = "UP"
+//Const variable not reassigned
+//accountId = "4354"
+
+console.log(accountId);
+console.table([accountId, accountName, accountPassword, accountCity, accountState])
